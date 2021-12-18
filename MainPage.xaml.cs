da0e399d3187985ed44e3a9dc5f46fe4d7655684
sammy310 <kipper098@gmail.com>
2021-12-18 21:14:56 +0900

    초기 커밋.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+
+// 빈 페이지 항목 템플릿에 대한 설명은 https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x412에 나와 있습니다.
+
+namespace MusicLyrics
+{
+    /// <summary>
+    /// 자체적으로 사용하거나 프레임 내에서 탐색할 수 있는 빈 페이지입니다.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size(650, 720));
+
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var mediaProperties = await MusicManager.GetMediaProperties();
+
+            BitmapImage image = new BitmapImage();
+            await image.SetSourceAsync(await mediaProperties.Thumbnail.OpenReadAsync());
+            thumbnail.Source = image;
+
+            titleText.Text = mediaProperties.Title;
+            artistText.Text = mediaProperties.Artist;
+
+            string lyrics = await MusicManager.GetMusicLyrics(mediaProperties);
+            if (lyrics != null)
+            {
+                lyricsText.Text = lyrics;
+            }
+        }
+    }
+}
---
+++ MusicManager.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Windows.Media.Control;
+using Windows.UI.Xaml.Controls;
+using HtmlAgilityPack;
+using System.Text.RegularExpressions;
+
+namespace MusicLyrics
+{
+    public class MusicManager
+    {
+        const string LyricsSite = "https://www.utamap.com/";
+        const string LyricsSearch_URL = "https://www.utamap.com/searchkasi.php?searchname={0}&word={1}";
+        const string LyricsSearch_Title = "title";
+        const string LyricsSearch_Artist = "artist";
+        const string FirstLyricsURLNode = "/html/body/table[3]/tr/td/table[1]/tr/td/a";
+        const string LyricsNode = "/html/body/div/div/table[2]/tr/td/table[3]/tr/td/table/tr[4]/td";
+
+        public static async Task<GlobalSystemMediaTransportControlsSessionMediaProperties> GetMediaProperties()
+        {
+            GlobalSystemMediaTransportControlsSessionManager sessions = await GlobalSystemMediaTransportControlsSessionManager.RequestAsync();
+            return await sessions.GetCurrentSession().TryGetMediaPropertiesAsync();
+        }
+
+        public static async Task<string> GetMusicLyrics(GlobalSystemMediaTransportControlsSessionMediaProperties properties)
+        {
+            string url = string.Format(LyricsSearch_URL, LyricsSearch_Title, properties.Title);
+            HtmlDocument document = await GetHtml(url);
+
+            var lyricsURLNode = document.DocumentNode.SelectSingleNode(FirstLyricsURLNode);
+            if (lyricsURLNode != null)
+            {
+                string lyricsURL = lyricsURLNode.GetAttributeValue("href", string.Empty);
+                if (lyricsURL.Length > 0)
+                {
+                    lyricsURL = LyricsSite + lyricsURL;
+                    HtmlDocument lyricsDocument = await GetHtml(lyricsURL);
+
+                    var lyricsNode = lyricsDocument.DocumentNode.SelectSingleNode(LyricsNode);
+                    if (lyricsNode != null)
+                    {
+                        Regex pattern = new Regex("<br>");
+                        string lyrics = pattern.Replace(lyricsNode.InnerHtml, "\n");
+                        return lyrics.Remove(lyrics.LastIndexOf("<!-- 歌詞 end -->")).Trim();
+                    }
+                }
+            }
+            return null;
+        }
+
+        static async Task<HtmlDocument> GetHtml(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                HttpContent content = response.Content;
+
+                HtmlDocument document = new HtmlDocument();
+                document.LoadHtml(await content.ReadAsStringAsync());
+
+                return document;
+            }
+        }
+    }
+}
